4676e74fd351abc388df35036cad01f84bbd9ea4
guido611 <guidodrechsel@yahoo.de>
2019-09-04 12:29:11 +0200

    read anaconda cloud credentials from separate file and bugfixes

--- upload_to_conda.sh
+++ upload_to_conda.sh
@@ -1,54 +1,83 @@
 #!/bin/bash
-set -e
+#set -e
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
-# manual steps to build and upload new package version:
+# ## MANUAL STEPS TO BUILD AND UPLOAD A NEW PACKAGE VERSION:
 # conda build . --python <python version>
-# conda build returns output path of the .tar.bz2 file
-# conda convert --platform all <path of tar.bz2 file>  -o $HOME/conda-bld/
-# anaconda login
+# conda build prints output path of the .tar.bz2 file
+# conda convert --platform all <path of tar.bz2 file>  -o $HOME/Anaconda3/conda-bld/
+# anaconda login --username <username> --password <password>
 # anaconda upload <path of tar.bz2 file> (have to do that for each tar file)
-# repeat upload for all plattforms and python versions
 # anaconda logout
 
 PYTHON_VERSION=(3.6 3.7)
-# TODO: read username and password from separate file that is gitingored
-# ANACONDA_USERNAME="username"
-# ANACONDA_PASSWORD="password"
+
+anaconda_credentials=( `cat "anaconda_credentials.txt" `)
+
+ANACONDA_USERNAME="$(echo ${anaconda_credentials[0]}|tr -d '\n\r')"
+ANACONDA_ORGANIZATION="$(echo ${anaconda_credentials[1]}|tr -d '\n\r')"
+ANACONDA_PASSWORD="$(echo ${anaconda_credentials[2]}|tr -d '\n\r')"
 
 
-echo "Building conda package ..."
-
-# building conda packages
 for py_version in "${PYTHON_VERSION[@]}"
 do
 	conda build . --python $py_version
 done
 
-# convert package to other platforms
+bit=$(uname -m)
+if [[ "$bit" == *"x86_64"* ]]; then
+        bit="64"
+else
+        bit="32"
+fi
+
+if [[ "$OSTYPE" == *"linux"* ]]; then
+        host_platform="linux-$bit"
+elif [[ "$OSTYPE" == *"darwin"* ]]; then
+        # Mac OSX
+        host_platform="osx-$bit"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+        host_platform="win-$bit"
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+        host_platform="win-$bit"
+elif [[ "$OSTYPE" == *"win"* ]]; then
+        # I'm not sure this can happen.
+        host_platform="win-$bit"
+else
+        echo "ERROR: Unknown platform!"
+        return
+fi
+
 cd ~
-platforms=( osx-64 linux-32 linux-64 win-32 win-64 )
-find $HOME/conda-bld/linux-64/ -name *.tar.bz2 | while read file
+if [ -d ./Anaconda3 ]; then
+	conda_build_path="Anaconda3/conda-bld"
+elif [ -d ./miniconda3 ]; then
+	conda_build_path="miniconda3/conda-bld"
+else
+	echo "ERROR: Could not determine directory of conda-build!"
+    return
+fi
+
+PLATFORMS=( osx-64 linux-32 linux-64 win-32 win-64 )
+find $HOME/$conda_build_path/$host_platform -name *.tar.bz2 | while read file
 do
-    echo $file
-    #conda convert --platform all $file  -o $HOME/conda-bld/
-    for platform in "${platforms[@]}"
+    for platform in "${PLATFORMS[@]}"
     do
-       conda convert --platform $platform $file  -o $HOME/conda-bld/
+       conda convert --platform $platform $file  -o $HOME/Anaconda3/conda-bld/
     done
 
 done
 
-#anaconda login --username $ANACONDA_USERNAME --password $ANACONDA_PASSWORD
-#anaconda login
+anaconda login --username $ANACONDA_USERNAME --password $ANACONDA_PASSWORD
 
-# upload packages to conda
-find $HOME/conda-bld/ -name *.tar.bz2 | while read file
+find $HOME/$conda_build_path/ -name *.tar.bz2 | while read file
 do
     echo $file
     anaconda upload $file
 done
 
-#anaconda logout
+anaconda logout
 
 echo "execution completed!"
