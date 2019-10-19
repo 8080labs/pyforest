7d294d6d22c2bfb29564c50bafd8f5e9fa68c369
tkrabel <tobiaskrabel@gmx.de>
2019-10-19 19:02:19 +0200

    refactor: add _ prefix to all function names

--- src/pyforest/user_specific_imports.py
+++ src/pyforest/user_specific_imports.py
@@ -12,62 +12,61 @@ TEMPLATE_TEXT = (
 )
 
 
-def clean_line(x: str) -> str:
+def _clean_line(x: str) -> str:
     return x.replace("\n", "").strip()
 
 
-def is_comment(x: str) -> bool:
+def _is_comment(x: str) -> bool:
     return x.startswith("#")
 
 
-def is_empty_line(x: str) -> bool:
+def _is_empty_line(x: str) -> bool:
     return x == ""
 
 
-def is_real_import(x: str) -> bool:
-    return not (is_comment(x) or is_empty_line(x))
+def _is_real_import(x: str) -> bool:
+    return not (_is_comment(x) or _is_empty_line(x))
 
 
-def keep_real_imports(import_statements: list) -> list:
+def _keep_real_imports(import_statements: list) -> list:
     return [
         import_statement
         for import_statement in import_statements
-        if is_real_import(import_statement)
+        if _is_real_import(import_statement)
     ]
 
 
-def clean_import_statements(import_statements: list) -> list:
+def _clean_import_statements(import_statements: list) -> list:
     cleaned_import_statements = [
-        clean_line(import_statement) for import_statement in import_statements
+        _clean_line(import_statement) for import_statement in import_statements
     ]
-    return keep_real_imports(cleaned_import_statements)
+    return _keep_real_imports(cleaned_import_statements)
 
 
-def _get_import_statetments_from_user_settings(user_settings_path: str) -> list:
+def _read_import_statetments_from_user_settings(user_settings_path: str) -> list:
     file_in = open(user_settings_path, "r")
-    import_statements = file_in.readlines()
-
-    return clean_import_statements(import_statements)
+    return file_in.readlines()
 
 
-def maybe_init_user_imports_directory(user_imports_path: pathlib.Path):
+def _maybe_init_user_imports_directory(user_imports_path: pathlib.Path):
     if not user_imports_path.parent.exists():
         user_imports_path.parent.mkdir(parents=True, exist_ok=True)
 
 
-def maybe_init_user_imports_file(user_imports_path: pathlib.Path) -> None:
+def _maybe_init_user_imports_file(user_imports_path: pathlib.Path) -> None:
     if not user_imports_path.exists():
-        maybe_init_user_imports_directory(user_imports_path)
+        _maybe_init_user_imports_directory(user_imports_path)
         user_imports_path.touch()
         user_imports_path.write_text(TEMPLATE_TEXT)
 
 
-def get_import_statetments_from_user_settings(user_imports_path) -> list:
-    maybe_init_user_imports_file(user_imports_path)
-    return _get_import_statetments_from_user_settings(user_imports_path)
+def _get_import_statetments_from_user_settings(user_imports_path) -> list:
+    _maybe_init_user_imports_file(user_imports_path)
+    import_statements = _read_import_statetments_from_user_settings(user_imports_path)
+    return _clean_import_statements(import_statements)
 
 
-def assign_imports_to_global_space(import_statements: list, globals_) -> None:
+def _assign_imports_to_global_space(import_statements: list, globals_) -> None:
     symbols = [import_statement.split()[-1] for import_statement in import_statements]
 
     for symbol, import_statement in zip(symbols, import_statements):
@@ -75,7 +74,7 @@ def assign_imports_to_global_space(import_statements: list, globals_) -> None:
 
 
 def _load_user_specific_imports(globals_):
-    user_import_statements = get_import_statetments_from_user_settings(
+    user_import_statements = _get_import_statetments_from_user_settings(
         USER_IMPORTS_PATH
     )
-    assign_imports_to_global_space(user_import_statements, globals_)
+    _assign_imports_to_global_space(user_import_statements, globals_)
