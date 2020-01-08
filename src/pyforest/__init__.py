00d896121ee83a474be19347321d5b79b6914c87
florianwetschoreck <florian.wetschoreck@gmail.com>
2020-01-08 17:19:29 +0100

    Import: adjust to not overwrite user symbols

--- src/pyforest/__init__.py
+++ src/pyforest/__init__.py
@@ -1,5 +1,26 @@
 # -*- coding: utf-8 -*-
+def get_user_symbols():
+    import inspect
+
+    for index, item in enumerate(inspect.stack()):
+        try:
+            name = item[0].f_globals["__name__"]
+            if name == "__main__":
+                return item[0].f_globals
+        except:  # __name__ attribute does not exist
+            pass
+    return {}
+
+
 from ._imports import *
+
+user_symbols = get_user_symbols()
+pyforest_imports = globals().copy().keys()
+
+for import_symbol in pyforest_imports:
+    # don't overwrite symbols of the user
+    if import_symbol not in user_symbols.keys():
+        user_symbols[import_symbol] = eval(import_symbol)
 
 
 #  set __version__ attribute
