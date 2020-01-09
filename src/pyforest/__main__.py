f45544d2a4b6fc9de92495fcf2575f5ba1f8c832
florianwetschoreck <florian.wetschoreck@gmail.com>
2020-01-09 12:10:07 +0100

    refactor

--- src/pyforest/__main__.py
+++ src/pyforest/__main__.py
@@ -1,4 +1,11 @@
-def main():
+from .utils import install_nbextension, install_labextension
+
+
+USAGE = """Usage: python -m pyforest install_extensions
+	installs notebook/lab extensions
+"""
+
+if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 2 or sys.argv[1] != "install_extensions":
@@ -7,40 +14,3 @@ def main():
 
     install_nbextension()
     install_labextension()
-
-
-def install_nbextension():
-    try:
-        from notebook import nbextensions
-    except ImportError:
-        # No notebook
-        return
-
-    print("Installing nbextension...")
-    nbextensions.install_nbextension_python("pyforest")
-    nbextensions.enable_nbextension_python("pyforest")
-
-
-def install_labextension():
-    try:
-        from jupyterlab import commands
-    except ImportError:
-        # No jupyterlab
-        return
-
-    from pathlib import Path
-
-    dir = Path(__file__).parent
-
-    print("Installing labextension...")
-    should_build = commands.install_extension(str(dir))
-    if should_build:
-        commands.build()
-
-
-USAGE = """Usage: python -m pyforest install_extensions
-	installs notebook/lab extensions
-"""
-
-if __name__ == "__main__":
-    main()
--- src/pyforest/utils.py
+++ src/pyforest/utils.py
@@ -9,3 +9,38 @@ def get_user_symbols():
         except:  # __name__ attribute does not exist
             pass
     return {}
+
+
+def install_nbextension():
+    print("Installing pyforest nbextension...")
+
+    try:
+        from notebook import nbextensions
+    except ImportError:
+        print("Error: Jupyter Notebook is not available")
+        return
+
+    nbextensions.install_nbextension_python("pyforest")
+    nbextensions.enable_nbextension_python("pyforest")
+    print("Successfully installed pyforest Jupyter Notebook nbextension")
+
+
+def install_labextension():
+    print("Installing pyforest labextension...")
+
+    try:
+        from jupyterlab import commands
+    except ImportError:
+        print("Error: Jupyter Lab is not available")
+        return
+
+    from pathlib import Path
+
+    dir = Path(__file__).parent
+
+    should_build = commands.install_extension(str(dir))
+    print("Successfully installed pyforest Jupyter Lab labextension")
+    if should_build:
+        print("Starting JupyterLab build")
+        commands.build()
+        print("Successfully built JupyterLab")
